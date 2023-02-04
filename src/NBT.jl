4a244fd6229f431beb5a022915af68e899899133
Ellie <intricatebread@gmail.com>
2023-02-03 23:41:16 -0300

    add writing, remove the 0 tag

--- src/NBT.jl
+++ src/NBT.jl
@@ -1,11 +1,12 @@
 module NBT
 
 using GZip: open
-export Tag, read_nbt, read_nbt_uncompressed
+export Tag, read_nbt, read_nbt_uncompressed, write_nbt, write_nbt_uncompressed
 
-struct Tag
-  name::Union{String, Nothing}
-  data::Any
+struct Tag{T}
+  id::UInt8
+  name::String
+  data::T
 end
 
 """
@@ -26,49 +27,103 @@ function read_nbt_uncompressed(stream::IO)::Tag
   _read_tag(read(stream, UInt8), stream)
 end
 
+"""
+    write_nbt(filename::String)
+
+Parses a tree of nested `Tag` objects, each of which contains a `name` and `data`, into an NBT file.
+"""
+function write_nbt(filename::String, tag::Tag)::IO
+  stream = open(filename)
+  _write_tag(tag, stream)
+  return stream
+end
+
+"""
+    write_nbt_uncompressed(io::io)
+
+Parses a tree of nested `Tag` objects, each of which contains a `name` and `data`, into an uncompressed NBT file.
+"""
+function write_nbt_uncompressed(tag::Tag)::IO
+  stream = IOBuffer();
+  _write_tag(tag, stream)
+  return stream
+end
+
 function _read_tag(id::UInt8, stream::IO; skipname::Bool=false)::Tag
-  types = (Int8, Int16, Int32, Int64, Float32, Float64,
-    Int8, nothing, nothing, nothing, Int32, Int64)
-  if id == 0x0
-    return Tag(nothing, nothing)
-  else
-    name = ""
-    if !skipname
-      namelength = ntoh(read(stream, UInt16))
-      for _ ∈ 1:namelength
-        name *= read(stream, Char)
-      end
+  types = (Int8, Int16, Int32, Int64, Float32, Float64, Int8, nothing, nothing, nothing, Int32, Int64)
+  name = ""
+  if !skipname
+    namelength = ntoh(read(stream, UInt16))
+    for _ ∈ 1:namelength
+      name *= read(stream, Char)
     end
+  end
 
-    if id < 0x7 # Singletons
-      return Tag(name, bswap(read(stream, types[id])))
+  if id < 0x7 # Singletons
+    return Tag(id, name, ntoh(read(stream, types[id])))
 
-    elseif id == 0x7 || id == 0xb || id == 0xc # Arrays
-      size = ntoh(read(stream, Int32))
-      return Tag(name, [read(stream, types[id]) for _ ∈ 1:size])
+  elseif id == 0x7 || id == 0xb || id == 0xc # Arrays
+    size = ntoh(read(stream, Int32))
+    return Tag(id, name, types[id][ntoh(read(stream, types[id])) for _ ∈ 1:size])
 
-    elseif id == 0x8 # String
-      size = ntoh(read(stream, UInt16))
-      string = ""
-      for _ ∈ 1:size string *= read(stream, Char) end
-      return Tag(name, string)
+  elseif id == 0x8 # String
+    size = ntoh(read(stream, UInt16))
+    string = ""
+    for _ ∈ 1:size string *= read(stream, Char) end
+    return Tag(id, name, string)
 
-    elseif id == 0x9 # Tag list
+  elseif id == 0x9 # Tag list
+    contentsid = read(stream, UInt8)
+    size = ntoh(read(stream, Int32))
+    if contentsid == 0x0 return Tag(id, name, Tag[]) end
+    return Tag(id, name, Tag[_read_tag(contentsid, stream; skipname=true) for _ ∈ 1:size])
+
+  elseif id == 0xa # Compound
+    tags = Tag[]
+    while true
       contentsid = read(stream, UInt8)
-      size = ntoh(read(stream, Int32))
-      return Tag(name, [_read_tag(contentsid, stream; skipname=true) for _ ∈ 1:size])
-
-    elseif id == 0xa # Compound
-      tags = Tag[]
-      while true
-        tag = _read_tag(read(stream, UInt8), stream)
-        push!(tags, tag)
-        if tag.name === nothing break end
-      end
-      return Tag(name, tags)
-
-    else throw(error("invalid tag id ($id); file may be corrupt"))
+      if contentsid == 0x0 break end
+      push!(tags, _read_tag(contentsid, stream))
     end
+    return Tag(id, name, tags)
+
+  else throw(error("invalid tag id ($id); file may be corrupt"))
+  end
+end
+
+function _write_tag(tag::Tag, stream::IO; skipname::Bool=false)
+  if !skipname
+    write(stream, tag.id)
+    write(stream, hton(Int16(length(tag.name))))
+    for c ∈ tag.name write(stream, c) end
+  end
+
+  if tag.id < 0x7
+    write(stream, hton(tag.data))
+
+  elseif tag.id == 0x7 || tag.id == 0xb || tag.id == 0xc
+    write(stream, hton(Int32(length(tag.data))))
+    for n ∈ tag.data write(stream, hton(n)) end
+
+  elseif tag.id == 0x8
+    write(stream, hton(UInt16(length(tag.data))))
+    for c ∈ tag.data write(stream, c) end
+
+  elseif tag.id == 0x9
+    write(stream, length(tag.data) > 0x0 ? first(tag.data).id : 0x0)
+    write(stream, hton(Int32(length(tag.data))))
+    for t ∈ tag.data
+      _write_tag(t, stream; skipname = true)
+    end
+
+  elseif tag.id == 0xa
+    for t ∈ tag.data
+      _write_tag(t, stream)
+    end
+    write(stream, 0x0)
+
+  else
+    throw(error("invalid tag id ($(tag.id)); tags may be corrupt"))
   end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,12 @@
 using NBT
+using GZip: open
 using Test
 
 @testset "NBT.jl" begin
-  @test hash(string(read_nbt("./xd.litematic"))) == 0x02af95d7ebdd5717
-  @test hash(string(read_nbt("./peepee alignment.litematic"))) == 0x4da76d1be9eb5f11
-  @test hash(string(read_nbt("./lamp_big.litematic"))) == 0xed0905100fb367a9
+  @test hash(string(read_nbt("./xd.litematic"))) == 0x67ce6d2ade66fa3e
+  @test hash(string(read_nbt("./peepee alignment.litematic"))) == 0x2d031b25729fa18d
+  @test hash(string(read_nbt("./lamp_big.litematic"))) == 0x7d8ee08d40304a7a
+  @test take!(write_nbt_uncompressed(read_nbt("./xd.litematic"))) == read(open("./xd.litematic"))
+  @test take!(write_nbt_uncompressed(read_nbt("./peepee alignment.litematic"))) == read(open("./peepee alignment.litematic"))
+  @test take!(write_nbt_uncompressed(read_nbt("./lamp_big.litematic"))) == read(open("./lamp_big.litematic"))
 end
