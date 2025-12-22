411c531e586b6ef0242115b44540bdc864747a56
Ellie <intricatebread@gmail.com>
2025-12-21 17:46:35 -0700

    update ancient tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,7 @@ Aqua.test_all(NBT)
     lasttime = time()
 
     io = IOBuffer()
-    tag = read(f, Tag)
+    tag = NBT.read(f)
     bytecount = NBT.write_nbt_uncompressed(io, tag)
     bytes = take!(io)
     newfile = tempname()
@@ -22,15 +22,7 @@ Aqua.test_all(NBT)
 
     @test length(bytes) == bytecount # Check if write returns the right number
     @test bytes == open(read, f) # Check if the uncompressed bytes match
-    @test hash(tag) == hash(read(newfile, Tag)) # Compare hash
-
-    # Compare hash after get_tags
-    @test hash(get_tags(tag, 0x9)) == hash(get_tags(read(newfile, Tag), 0x9))
-    @test hash(get_tags(tag, "x")) == hash(get_tags(read(newfile, Tag), "x"))
-    @test (t = get_tags(tag, 0x3, depth=1);
-      tag[0x3] === (length(t) == 0 ? nothing : first(t)))
-    @test (t = get_tags(tag, "Version", depth=1);
-      tag["Version"] === (length(t) == 0 ? nothing : first(t)))
+    @test hash(tag) == hash(NBT.read(newfile)) # Compare hash
 
     println(" (", round(1000(time() - lasttime); digits=2), "ms)")
   end
